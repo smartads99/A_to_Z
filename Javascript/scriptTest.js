fd3ad5c7713223a12f9fb54fac0695a4d9158d13
Boobesh <boobeshsuresh16@gmail.com>
2024-01-21 23:06:20 +0530

    21-01-2024

---
+++ Javascript/scriptTest.js
@@ -0,0 +1,22 @@
+// let userName="Boobesh";
+// let age=19
+
+// console.log("Name :"+userName);
+// console.log("Age :"+age)
+
+
+// let Person={
+
+//     Name:"Boobesh",
+//     age:19,
+//     gender:"Male",
+//     address:"Cuddalore"
+// }
+
+// // Person.Name="Boobeshs";
+
+// console.log(Person)
+
+
+console.log("1 for Add, 2 for Sub, 3 Mul")
+
